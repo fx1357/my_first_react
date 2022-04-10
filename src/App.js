96eef303c2f1e09c9279a88bedc447b09e23adf5
Y.H.JEONG <fx13577@gmail.com>
2022-04-10 11:40:48 +0900

    add networking & DataParsing

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-// import logo from './logo.svg';
-// import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>ㅓㅑㅝㅜㅑㅓㅜㅇㄹㄴ이ㅏㅜㄹ</h1>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// 모듈을 불러오는 부분
 import React, { useState } from 'react';
+import axios from 'axios';
+
+
+
+
 
 const App = () => {
 
-  // 함수, 상수, 변수, 상태선언
+// 1. 데이타를 담을 그릇 생성
+const [movies, setMovies] = useState([])
 
-  const [keyword, setKeyword] = useState(true)
+const getMovies = async () => {
+  // 네트워킹 및 데이터 파싱
 
+  try {
+    const res = await axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=8597e491ed6e80f0de12e349eb60ea6e&language=en-US&page=1")
+    console.log(res.data.results)
+    setMovies(res.data.results)
   
-  const changeWord = () => { 
-     setKeyword(false)     
+  } catch(err) {
+    console.log(err)
   }
 
+}
 
+const clearMovie = () => {
+ 
+   setMovies([])
+}
 
-
-  // 화면에 보여지는 부분
   return (
     <div>
-      <h1>
-        {keyword ? "참이다" : "거짓"}
-      </h1>
-      <button onClick={() => changeWord()}>button</button>
+      {/* <h1>{movies.length}</h1> */}
+      {movies.map(movie => (
+        <div>
+          <h1>{movie.original_title}</h1>
+          <span>{movie.overview}</span>
+        </div>
+
+      ))}
+      <button onClick={() => getMovies()}>데이타 가져오기</button>
+      <button onClick={() => clearMovie()}>데이타 비우기</button>
     </div>
   );
 };
 
 export default App;
-
-
-// 1. 위에서 아래
-// 2. = 오른쪽에서 왼쪽
-// 3. () 함수들의 모음, 함수 호출은 .으로 호출
