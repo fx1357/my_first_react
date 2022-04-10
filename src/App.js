780b8e3c7535da6bd05fd26431e7a31bfc07aa98
Y.H.JEONG <fx13577@gmail.com>
2022-04-10 12:23:31 +0900

    add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,49 +1,51 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
-
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 
 
 
 const App = () => {
+  const [articles, setArticles] = useState([])
 
-// 1. 데이타를 담을 그릇 생성
-const [movies, setMovies] = useState([])
-
-const getMovies = async () => {
-  // 네트워킹 및 데이터 파싱
-
-  try {
-    const res = await axios.get("https://api.themoviedb.org/3/movie/now_playing?api_key=8597e491ed6e80f0de12e349eb60ea6e&language=en-US&page=1")
-    console.log(res.data.results)
-    setMovies(res.data.results)
-  
-  } catch(err) {
-    console.log(err)
+  const getDate = async () => {
+          
+    try{
+      const {data} = await axios.get("https://newsapi.org/v2/everything?q=tesla&from=2022-03-10&sortBy=publishedAt&apiKey=f1c87e2bb86248de9f9492e513f93e1f")  
+      console.log(data)
+      setArticles(data.articles)
+    }catch(e){
+      console.log(e)
+    }
+    
   }
 
-}
+  useEffect(() => {
+    getDate()
+  }, [])
 
-const clearMovie = () => {
- 
-   setMovies([])
-}
 
+  
   return (
-    <div>
-      {/* <h1>{movies.length}</h1> */}
-      {movies.map(movie => (
-        <div>
-          <h1>{movie.original_title}</h1>
-          <span>{movie.overview}</span>
-        </div>
-
-      ))}
-      <button onClick={() => getMovies()}>데이타 가져오기</button>
-      <button onClick={() => clearMovie()}>데이타 비우기</button>
-    </div>
+    <Container>
+      <Row>
+       {articles.map(article => (
+        <Col>
+         <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={article.urlToImage ? article.urlToImage : require("./assets/emptyImage.png")} />
+          <Card.Body>
+           <Card.Title>{article.title.slice(0, 50)}</Card.Title>
+           <Card.Text>
+           {article.description.slice(0,100)}
+         </Card.Text>
+         <Button variant="primary">Go somewhere</Button>
+       </Card.Body>
+     </Card>
+     </Col>
+    ))}
+    </Row>
+    </Container>
   );
-};
+}
 
 export default App;
